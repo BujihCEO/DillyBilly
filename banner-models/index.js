0d4ad4fa06c60fffdcdc025848f28c5b1e601369
BujihCEO <shop.bujih@gmail.com>
2024-11-10 15:56:16 -0300

    Update index.js

--- banner-models/index.js
+++ banner-models/index.js
@@ -1,15 +1,14 @@
-console.log('banner-models');
-()=> {
+(() => {
     const target = document.querySelector('.product-section');
     if (target) {
         const img = new Image();
-        img.onload = ()=> {
+        img.onload = () => {
             const box = document.createElement('div');
-            box.style = `width: 100%; padding: 10px; box-sizing: border-box; display: flex; justify-content: center;`
+            box.style = `width: 100%; padding: 10px; box-sizing: border-box; display: flex; justify-content: center;`;
             img.style = 'max-width: 100%;';
             box.append(img);
             target.insertBefore(box, target.children[0]);
         };
         img.src = 'https://montink.s3.amazonaws.com/produto_imagens/258943/phpsJbPpm.png';
     }
-}
+})();
