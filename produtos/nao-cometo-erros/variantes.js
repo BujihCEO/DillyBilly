8c0c241a9b9d4c0112f391aaa8f84ae37c68b018
BujihCEO <shop.bujih@gmail.com>
2024-11-15 14:39:03 -0300

    Update variantes.js

--- produtos/nao-cometo-erros/variantes.js
+++ produtos/nao-cometo-erros/variantes.js
@@ -18,22 +18,22 @@ export const variantes = {
         {id:"bnq7x", c:"#FF7BC7"}, // ROSA
         {id:"ge2n0", c:"#7352BF"}, // ROXO 
     ],
-    'Ecobag': [
-        {id:"", c:"#4294C2"}, // AZUL
-        {id:"", c:"#3167A4"}, // AZUL ESCURO
-        {id:"", c:"#48A594"}, // VERDE
-        {id:"", c:"#337050"}, // VERDE ESCURO
-        {id:"", c:"#FF4C42"}, // LARANJA
-        {id:"", c:"#FF7BC7"}, // ROSA
-        {id:"", c:"#7352BF"}, // ROXO 
-    ],
-    'Caneca': [
-        {id:"", c:"#4294C2"}, // AZUL
-        {id:"", c:"#3167A4"}, // AZUL ESCURO
-        {id:"", c:"#48A594"}, // VERDE
-        {id:"", c:"#337050"}, // VERDE ESCURO
-        {id:"", c:"#FF4C42"}, // LARANJA
-        {id:"", c:"#FF7BC7"}, // ROSA
-        {id:"", c:"#7352BF"}, // ROXO 
-    ]
+    // 'Ecobag': [
+    //     {id:"", c:"#4294C2"}, // AZUL
+    //     {id:"", c:"#3167A4"}, // AZUL ESCURO
+    //     {id:"", c:"#48A594"}, // VERDE
+    //     {id:"", c:"#337050"}, // VERDE ESCURO
+    //     {id:"", c:"#FF4C42"}, // LARANJA
+    //     {id:"", c:"#FF7BC7"}, // ROSA
+    //     {id:"", c:"#7352BF"}, // ROXO 
+    // ],
+    // 'Caneca': [
+    //     {id:"", c:"#4294C2"}, // AZUL
+    //     {id:"", c:"#3167A4"}, // AZUL ESCURO
+    //     {id:"", c:"#48A594"}, // VERDE
+    //     {id:"", c:"#337050"}, // VERDE ESCURO
+    //     {id:"", c:"#FF4C42"}, // LARANJA
+    //     {id:"", c:"#FF7BC7"}, // ROSA
+    //     {id:"", c:"#7352BF"}, // ROXO 
+    // ]
 }
