62bd766e395840aef4f0fe73988cd3926c8bcae1
BujihCEO <shop.bujih@gmail.com>
2024-11-19 15:39:44 -0300

    .

---
+++ produtos/sempre-me-atraso/variantes.js
@@ -0,0 +1,39 @@
+// Sempre me atraso
+export const variantes = {
+    'Camiseta': [
+        {id:"ik274", c:"#4294C2"}, // AZUL
+        {id:"q4pvx", c:"#3167A4"}, // AZUL ESCURO
+        {id:"y88uy", c:"#48A594"}, // VERDE
+        {id:"3l00o", c:"#337050"}, // VERDE ESCURO
+        {id:"e5vbw", c:"#FF4C42"}, // LARANJA
+        {id:"q49at", c:"#FF7BC7"}, // ROSA
+        {id:"kfgon", c:"#7352BF"}, // ROXO 
+    ],
+    'Baby-Look': [
+        {id:"mf3m8", c:"#4294C2"}, // AZUL
+        {id:"rbrik", c:"#3167A4"}, // AZUL ESCURO
+        {id:"yd8yg", c:"#48A594"}, // VERDE
+        {id:"fydvn", c:"#337050"}, // VERDE ESCURO
+        {id:"wmj4a", c:"#FF4C42"}, // LARANJA
+        {id:"2fj1q", c:"#FF7BC7"}, // ROSA
+        {id:"zv56o", c:"#7352BF"}, // ROXO 
+    ],
+    // 'Ecobag': [
+    //     {id:"", c:"#4294C2"}, // AZUL
+    //     {id:"", c:"#3167A4"}, // AZUL ESCURO
+    //     {id:"", c:"#48A594"}, // VERDE
+    //     {id:"", c:"#337050"}, // VERDE ESCURO
+    //     {id:"", c:"#FF4C42"}, // LARANJA
+    //     {id:"", c:"#FF7BC7"}, // ROSA
+    //     {id:"", c:"#7352BF"}, // ROXO 
+    // ],
+    // 'Caneca': [
+    //     {id:"", c:"#4294C2"}, // AZUL
+    //     {id:"", c:"#3167A4"}, // AZUL ESCURO
+    //     {id:"", c:"#48A594"}, // VERDE
+    //     {id:"", c:"#337050"}, // VERDE ESCURO
+    //     {id:"", c:"#FF4C42"}, // LARANJA
+    //     {id:"", c:"#FF7BC7"}, // ROSA
+    //     {id:"", c:"#7352BF"}, // ROXO 
+    // ]
+}
