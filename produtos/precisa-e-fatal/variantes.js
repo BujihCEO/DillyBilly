52d279cad040de556dee144ca85afa499fe5d58d
BujihCEO <shop.bujih@gmail.com>
2024-11-20 19:36:16 -0300

    .

---
+++ produtos/precisa-e-fatal/variantes.js
@@ -0,0 +1,39 @@
+// Precisa e fatal
+export const variantes = {
+    'Camiseta': [
+        {id:"lm072", c:"#4294C2"}, // AZUL
+        {id:"j285h", c:"#3167A4"}, // AZUL ESCURO
+        {id:"317kp", c:"#48A594"}, // VERDE
+        {id:"n483g", c:"#337050"}, // VERDE ESCURO
+        {id:"r680r", c:"#FF4C42"}, // LARANJA
+        {id:"uopji", c:"#FF7BC7"}, // ROSA
+        {id:"l0nu6", c:"#7352BF"}, // ROXO 
+    ],
+    'Baby-Look': [
+        {id:"i073h", c:"#4294C2"}, // AZUL
+        {id:"unl4o", c:"#3167A4"}, // AZUL ESCURO
+        {id:"qqxn1", c:"#48A594"}, // VERDE
+        {id:"bqf6o", c:"#337050"}, // VERDE ESCURO
+        {id:"u67wj", c:"#FF4C42"}, // LARANJA
+        {id:"e8ws9", c:"#FF7BC7"}, // ROSA
+        {id:"d8jlg", c:"#7352BF"}, // ROXO 
+    ],
+    // 'Ecobag': [
+    //     {id:"", c:"#4294C2"}, // AZUL
+    //     {id:"", c:"#3167A4"}, // AZUL ESCURO
+    //     {id:"", c:"#48A594"}, // VERDE
+    //     {id:"", c:"#337050"}, // VERDE ESCURO
+    //     {id:"", c:"#FF4C42"}, // LARANJA
+    //     {id:"", c:"#FF7BC7"}, // ROSA
+    //     {id:"", c:"#7352BF"}, // ROXO 
+    // ],
+    // 'Caneca': [
+    //     {id:"", c:"#4294C2"}, // AZUL
+    //     {id:"", c:"#3167A4"}, // AZUL ESCURO
+    //     {id:"", c:"#48A594"}, // VERDE
+    //     {id:"", c:"#337050"}, // VERDE ESCURO
+    //     {id:"", c:"#FF4C42"}, // LARANJA
+    //     {id:"", c:"#FF7BC7"}, // ROSA
+    //     {id:"", c:"#7352BF"}, // ROXO 
+    // ]
+}
