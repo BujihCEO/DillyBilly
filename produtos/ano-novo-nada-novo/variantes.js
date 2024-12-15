4ddfeb5b517628f15449dca57a6f7bd3bf11191c
BujihCEO <shop.bujih@gmail.com>
2024-12-15 12:59:33 -0300

    .

---
+++ produtos/ano-novo-nada-novo/variantes.js
@@ -0,0 +1,21 @@
+// Ano novo nada novo
+export const variantes = {
+    'Camiseta': [
+        {id:"wbrha", c:"#4294C2"}, // AZUL
+        {id:"w8w2p", c:"#3167A4"}, // AZUL ESCURO
+        {id:"3ppux", c:"#48A594"}, // VERDE
+        {id:"etk46", c:"#337050"}, // VERDE ESCURO
+        {id:"s9x5r", c:"#FF4C42"}, // LARANJA
+        {id:"umx0o", c:"#FF7BC7"}, // ROSA
+        {id:"mozca", c:"#7352BF"}, // ROXO 
+    ],
+    'Baby-Look': [
+        {id:"ckprg", c:"#4294C2"}, // AZUL
+        {id:"lwzzk", c:"#3167A4"}, // AZUL ESCURO
+        {id:"std6s", c:"#48A594"}, // VERDE
+        {id:"79axl", c:"#337050"}, // VERDE ESCURO
+        {id:"1ar4y", c:"#FF4C42"}, // LARANJA
+        {id:"k0cxk", c:"#FF7BC7"}, // ROSA
+        {id:"8a553", c:"#7352BF"}, // ROXO 
+    ],
+}
